e69e4277172fe3df3705873f454272f4039f7bef
Igor Batukhtin <131672373+ibatukhtin@users.noreply.github.com>
2024-05-04 15:31:26 +0500

    font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import markdownIntegration from '@astropub/md';
 import preact from "@astrojs/preact";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [markdownIntegration(), preact(), react()],
+  integrations: [preact(), react()],
   markdown: {
     remarkPlugins: [],
     rehypePlugins: []
