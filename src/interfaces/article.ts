edb40ce4fb91914f2ea6602bef7499378c67dd76
Igor Batukhtin <131672373+ibatukhtin@users.noreply.github.com>
2024-05-04 11:26:49 +0500

    header, scss

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -1,16 +1,73 @@
 export default interface Article {
   id: number;
   attributes: {
+    createdAt: any;
+    updatedAt: any;
+    publishedAt: any;
     title: string;
     description: string;
+    category: {
+      data: Category
+    };
+    author: {
+      data: Author
+    };
     content: string;
     url: string;
     slug: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
     cover?: Cover;
-  };
+    relatedArticles?: {
+      data: Article[]
+    };
+    static_img_url?: string;
+    static_date?: any;
+    tags?: {
+      data: Tag[]
+    };
+    seo: Seo;
+    seo_text: string;
+  }
+}
+
+interface Seo {
+  metaTitle: string;
+  metaDescription: string;
+  metaKeywords: string;
+}
+
+export interface Author {
+  attributes: {
+    name: string;
+    slug: string;
+    caption: string;
+    caption_line_2: string;
+    email: string;
+    link: string;
+    avatar: Avatar;
+    seo: Seo;
+    seo_text: string;
+  }
+}
+
+export interface Category {
+  attributes: {
+    name: string;
+    slug: string;
+    articles: Article;
+    seo: Seo;
+    seo_text: string;
+  }
+}
+
+export interface Tag {
+  attributes: {
+    name: string;
+    slug: string;
+    color: string;
+    articles: Article;
+    seo: Seo;
+    seo_text: string;
+  }
 }
 
 export interface Cover {
@@ -29,6 +86,14 @@ export interface Cover {
           height: number;
         }
       }
+    }
+  }
+}
+
+interface Avatar {
+  data: {
+    attributes: {
+      url: string;
     }
   }
 }
