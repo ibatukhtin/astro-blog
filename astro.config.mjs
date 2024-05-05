6b432f7be58d7322283953efcb0a4a9306039ab3
Igor Batukhtin <131672373+ibatukhtin@users.noreply.github.com>
2024-05-05 12:41:26 +0500

    shadcn-ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    preact(),
     react(),
     tailwind({
       applyBaseStyles: false,
