b848058d9f9bbbf816ae198b790f2060a9aea0a3
Igor Batukhtin <131672373+ibatukhtin@users.noreply.github.com>
2024-05-08 19:25:07 +0500

    show more 5

---
+++ src/components/ShowMore.tsx
@@ -0,0 +1,47 @@
+import { useRef, useState } from "react";
+
+function ShowMore() {
+  const elRef = useRef<HTMLButtonElement>(null);
+
+  const posts = document.querySelectorAll('.blog-list-item');
+  const [visiblePosts, setVisiblePosts] = useState(18);
+
+  const showMorePosts = () => {
+    posts.forEach((post, index) => {
+      if (index < visiblePosts) {
+        post.classList.add('show');
+      } else {
+        post.classList.remove('show');
+      }
+    });
+
+    if (elRef.current) {
+      if (visiblePosts < posts.length) {
+        elRef.current.style.display = 'block';
+      } else {
+        elRef.current.style.display = 'none';
+      }
+    }
+  }
+
+  const handleClick = () => {
+    setVisiblePosts(visiblePosts + 18);
+  }
+
+  showMorePosts();
+
+  return (
+    <div style={ { display: 'flex', justifyContent: 'center' } }>
+      <button
+        ref={ elRef }
+        id="show-more"
+        className="tp-button-outline"
+        onClick={() => handleClick()}
+      >
+        Show more
+      </button>
+    </div>
+  )
+}
+
+export default ShowMore;
