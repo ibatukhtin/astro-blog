42ec1299dcda4a6cd596b9cd788a82d117289cb6
Igor Batukhtin <131672373+ibatukhtin@users.noreply.github.com>
2024-05-13 17:57:37 +0500

    preact new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 import react from "@astrojs/react";
-// import preact from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // preact({ compat: true }),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [preact({
+    compat: true
+  }),
+  // react(),
+  tailwind({
+    applyBaseStyles: false
+  }), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   markdown: {
     remarkPlugins: [],
     rehypePlugins: []
-  },
+  }
 });
