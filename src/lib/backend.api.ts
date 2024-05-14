51bf36604ba00c1e5cd66ac3e6ce35b8968c2960
Igor Batukhtin <131672373+ibatukhtin@users.noreply.github.com>
2024-05-14 15:01:32 +0500

    refactoring

--- src/lib/backend.api.ts
+++ src/lib/backend.api.ts
@@ -1,4 +1,8 @@
+export function getEnvironmentVariable(varName: string, defaultValue: string): string {
+  return import.meta.env[varName] ?? defaultValue;
+}
+
 export function getRequestUrl(urlPath: string) {
-  const API_URL = 'https://triplay-backend-dot-triplay-development.ey.r.appspot.com/api'
+  const API_URL = getEnvironmentVariable('API_URL', 'https://triplay-backend-dot-triplay-development.ey.r.appspot.com/api');
   return `${ API_URL }${ urlPath }`;
 }
--- src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'nanostores';
-
-export const showPosts = atom(12);
-export const morePosts = () => {
-  showPosts.set(showPosts.get() + 12);
-}
